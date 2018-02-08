87b6b6e7bc6fa66feee64c9398c2c13cb5fcbacd
Cole Arendt <cole@rstudio.com>
2018-02-08 05:38:04 +0000

    Update postgres

---
+++ cluster/postgres/config_postgres_krb5.sh
@@ -0,0 +1,18 @@
+
+
+# set-up kerb stuff
+kadmin -p ubuntu/admin -w ubuntu -q "addprinc -randkey postgres/k-postgres"
+kadmin -p ubuntu/admin -w ubuntu -q "ktadd -k /etc/krb5.keytab postgres/k-postgres"
+
+kinit -k -t /etc/krb5.keytab  postgres/k-postgres
+
+# configure database
+su postgres -c "psql -c 'CREATE ROLE \"postgres/k-postgres@DOCKER-RSTUDIO.COM\" SUPERUSER LOGIN'"
+
+echo "krb_server_keyfile = '/etc/krb5.keytab'" >> /var/lib/postgresql/data/postgresql.conf
+echo "host all all 0.0.0.0/0 gss include_realm=1 krb_realm=DOCKER-RSTUDIO.COM" >> /var/lib/postgresql/data/pg_hba.conf
+
+# reload database params
+su postgres -c "/usr/lib/postgresql/10/bin/pg_ctl reload"
+
+
