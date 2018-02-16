820baa8cb81107a94573146c77a310d14d2dbc48
Cole Arendt <cole@rstudio.com>
2018-02-16 11:17:26 +0000

    Seed database users

--- cluster/postgres/config_postgres_krb5.sh
+++ cluster/postgres/config_postgres_krb5.sh
@@ -1,3 +1,4 @@
+# expects to be executed as the postgres user
 
 # set-up kerb stuff
 kadmin -p ubuntu/admin -w ubuntu -q "addprinc -randkey postgres/k-postgres@DOCKER-RSTUDIO.COM"
@@ -8,10 +9,7 @@ kinit -k -t /etc/krb5.keytab  postgres/k-postgres
 chown postgres:postgres /etc/krb5.keytab
 
 # configure database
-su postgres -c "psql -c 'CREATE ROLE \"postgres/k-postgres@DOCKER-RSTUDIO.COM\" SUPERUSER LOGIN'"
-
-#echo "krb_server_keyfile = '/etc/krb5.keytab'" >> /var/lib/postgresql/data/postgresql.conf
-#echo "host all all 0.0.0.0/0 gss include_realm=1 krb_realm=DOCKER-RSTUDIO.COM" >> /var/lib/postgresql/data/pg_hba.conf
+psql -c 'CREATE ROLE \"postgres/k-postgres@DOCKER-RSTUDIO.COM\" SUPERUSER LOGIN'
 
 chown postgres:postgres /tmp/pg_hba.conf 
 chown postgres:postgres /tmp/postgresql.conf
@@ -21,11 +19,9 @@ cp /tmp/postgresql.conf /var/lib/postgresql/data/postgresql.conf
 
 # seed users in the database
 # - presumes that /tmp/users is still around
-#RUN awk ' { pass=system("openssl passwd -1 "$2); system("useradd -m -p "pass" -s /bin/bash "$1) } ' /tmp/users
-#awk ' { system("psql -h localhost -d postgres -c '
-#awk ' { system("psql -h localhost -d postgres -p 49159 -c \" SELECT '\''test'\'' \"")}' ~/users 
+awk ' { system("psql -h localhost -d postgres -p 5432 -c '\''CREATE ROLE \""$1"@DOCKER-RSTUDIO.COM\" WITH LOGIN;'\''") } ' /tmp/users
 
 # reload database params
-su postgres -c "/usr/lib/postgresql/${PG_MAJOR}/bin/pg_ctl reload"
+usr/lib/postgresql/${PG_MAJOR}/bin/pg_ctl reload
 
 
