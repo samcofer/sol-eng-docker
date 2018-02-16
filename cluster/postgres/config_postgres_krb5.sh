58f205e91b3110d0a98d1da1a6ef55fc5202fff8
Cole Arendt <cole@rstudio.com>
2018-02-16 03:06:34 +0000

    Clean up postgres image

--- cluster/postgres/config_postgres_krb5.sh
+++ cluster/postgres/config_postgres_krb5.sh
@@ -1,11 +1,11 @@
 
-
 # set-up kerb stuff
 kadmin -p ubuntu/admin -w ubuntu -q "addprinc -randkey postgres/k-postgres@DOCKER-RSTUDIO.COM"
 kadmin -p ubuntu/admin -w ubuntu -q "ktadd -k /etc/krb5.keytab postgres/k-postgres@DOCKER-RSTUDIO.COM"
 kadmin -p ubuntu/admin -w ubuntu -q "addprinc -randkey host/k-postgres@DOCKER-RSTUDIO.COM"
 
 kinit -k -t /etc/krb5.keytab  postgres/k-postgres
+chown postgres:postgres /etc/krb5.keytab
 
 # configure database
 su postgres -c "psql -c 'CREATE ROLE \"postgres/k-postgres@DOCKER-RSTUDIO.COM\" SUPERUSER LOGIN'"
@@ -19,7 +19,13 @@ chown postgres:postgres /tmp/postgresql.conf
 cp /tmp/pg_hba.conf /var/lib/postgresql/data/pg_hba.conf
 cp /tmp/postgresql.conf /var/lib/postgresql/data/postgresql.conf
 
+# seed users in the database
+# - presumes that /tmp/users is still around
+#RUN awk ' { pass=system("openssl passwd -1 "$2); system("useradd -m -p "pass" -s /bin/bash "$1) } ' /tmp/users
+#awk ' { system("psql -h localhost -d postgres -c '
+#awk ' { system("psql -h localhost -d postgres -p 49159 -c \" SELECT '\''test'\'' \"")}' ~/users 
+
 # reload database params
-su postgres -c "/usr/lib/postgresql/10/bin/pg_ctl reload"
+su postgres -c "/usr/lib/postgresql/${PG_MAJOR}/bin/pg_ctl reload"
 
 
