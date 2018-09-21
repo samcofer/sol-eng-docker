eab2d7cf2a3aa1c2bdc7b81fdae5201fc4481ced
Cole Arendt <cole@rstudio.com>
2018-09-21 14:34:17 +0000

    add cgi script example

---
+++ cluster/apache-kerb/cgi_krb.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+######################################################################
+#
+# Example CGI script that uses Kerberos credentials cached by
+# mod_auth_kerb compiled with caching option.
+#
+# Submitted by: Von Welch <vwelch@ncsa.uiuc.edu>
+#
+# mod_auth_kerb - Daniel Henninger <daniel@ncsu.edu>
+#
+######################################################################
+
+#PATH=/usr/heimdal/bin:$PATH
+
+# Output HTML header
+echo Content-type: text/plain
+echo
+
+# $REMOTE_USER should be set by httpd
+if [ -z "$REMOTE_USER" ]; then
+    echo '$REMOTE_USER not set.'
+    exit 1
+fi
+
+echo "REMOTE_USER is $REMOTE_USER"
+
+if [ -z "$KRB5CCNAME" ]; then
+    echo 'Kerberos credential cache name $KRB5CCNAME does not exist.'
+    exit 1
+fi
+
+# Do Kerberos stuff
+klist
+
+exit 0
+
