5925d71b95203c535e7540b11a35aeb0b3568e97
Cole Arendt <cole.arendt@outlook.com>
2018-10-19 11:41:47 -0400

    add shell script protection against missing sysdeps

--- cluster/cert_get.sh
+++ cluster/cert_get.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if ( [[ -z `which curl` ]] || [[ -z `which jq` ]] ); then 
+  echo 'ERROR: `curl` or `jq` not found. Both are required'; 
+  exit 2;
+fi
+
 curl -d @cert_config.json http://cfssl:8888/api/v1/cfssl/newcert > new_cert.json
 
 cat new_cert.json | jq -r '.result.certificate' > $(hostname).crt
