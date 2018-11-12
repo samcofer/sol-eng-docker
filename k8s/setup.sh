f1cf52c3229345549f3e814444411e2b277d50f4
Cole Arendt <cole.arendt@outlook.com>
2018-11-12 14:37:46 -0500

    add example configmap

--- k8s/setup.sh
+++ k8s/setup.sh
@@ -1,3 +1,5 @@
 kubectl create namespace rstudio
 kubectl create serviceaccount --namespace=rstudio job-launcher
 kubectl create rolebinding job-launcher-admin --clusterrole=cluster-admin --user=job-launcher --namespace=rstudio
+
+kubectl create configmap --namespace=rstudio ldap-users --from-file ../cluster/users.ldif
