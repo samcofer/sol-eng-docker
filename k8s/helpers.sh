cbd6695bd6aadc23de763e195d273252839aef81
Cole Arendt <cole.arendt@outlook.com>
2018-11-13 17:00:44 -0500

    add helper for the rstudio namespace

---
+++ k8s/helpers.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# alias to make it easier to reference the rstudio namespace
+alias kr="kubectl --namespace=rstudio"
+
+# TODO: Helpers for `port-forward` and `get -o yaml` that use a regex?
