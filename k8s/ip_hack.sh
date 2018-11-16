d6edaa0f79f6e3a49034b51fcc278b7e67aba1f8
Cole Arendt <cole.arendt@outlook.com>
2018-11-15 22:36:44 -0500

    start an ip address hack as a short term fix for nfs nonsense

---
+++ k8s/ip_hack.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z `which jq` ]; then
+  echo 'ERROR: `jq` was not found on your `PATH`, and it is required'
+  exit 1
+fi
+
+ip_addr=`kubectl --namespace=rstudio get service nfs01 -o json | jq -r '.spec.clusterIP'`
+res=$?
+if [ $res ]; then
+  echo "ERROR getting IP address: ${ip_addr}"
+  exit 1
+fi
+
+# ... inject this IP into the necessary files...?
+# with an _UGLY_ regex... I really need to learn sed better
+
+cat cluster/launcher-rsp/launcher-mounts | \
+	 sed "s/Host: [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*/Host: ${ip_addr}/" \
+	 > cluster/launcher-rsp/launcher-mounts
+
