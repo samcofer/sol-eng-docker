ad83d266dbf394d63e4e6af50b039bdad687e23a
Cole Arendt <cole.arendt@outlook.com>
2018-11-16 00:07:39 -0500

    fix and improve the ip_hack script

--- k8s/ip_hack.sh
+++ k8s/ip_hack.sh
@@ -15,7 +15,16 @@ fi
 # ... inject this IP into the necessary files...?
 # with an _UGLY_ regex... I really need to learn sed better
 
-cat cluster/launcher-rsp/launcher-mounts | \
-	 sed "s/Host: [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*/Host: ${ip_addr}/" \
-	 > cluster/launcher-rsp/launcher-mounts
+echo "Found IP address: ${ip_addr}"
 
+echo "Replacing in cluster/launcher-rsp/launcher-mounts"
+cat cluster/launcher-rsp/launcher-mounts | sed "s/Host: [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*/Host: ${ip_addr}/" > ...tmpfile
+mv ...tmpfile cluster/launcher-rsp/launcher-mounts
+
+echo "Replacing in cluster/launcher-ldap/launcher-mounts"
+cat cluster/launcher-ldap/launcher-mounts | sed "s/Host: [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*/Host: ${ip_addr}/" > ...tmpfile
+mv ...tmpfile cluster/launcher-ldap/launcher-mounts
+
+echo "Replacing in k8s/pv.yml"
+cat k8s/pv.yml | sed "s/server: [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*/server: ${ip_addr}/" > ...tmpfile
+mv ...tmpfile k8s/pv.yml
