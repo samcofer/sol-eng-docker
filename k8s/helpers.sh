2a317730da7292bb3edf12f34061a2c6de7d451d
Cole Arendt <cole.arendt@outlook.com>
2019-01-25 23:33:18 -0500

    add helpers to change the image prefix

--- k8s/helpers.sh
+++ k8s/helpers.sh
@@ -3,4 +3,29 @@
 # alias to make it easier to reference the rstudio namespace
 alias kr="kubectl --namespace=rstudio"
 
+# replace the docker image names in k8s files
+function set_k8s_gce() {
+  export K8S_IMAGE_PREFIX=us.gcr.io/rstudio-soleng
+}
+
+function set_k8s_local() {
+  export K8S_IMAGE_PREFIX=rstudio
+}
+
+function set_image_prefix() {
+  sed -i .bk -E "s/(image:).*\/(.*)$/\1 ${K8S_IMAGE_PREFIX//\//\\/}\/\2/" $1
+}
+
+function set_all_image_prefixes() {
+  set_image_prefix ./k8s/launcher-ldap.yml
+  set_image_prefix ./k8s/launcher.yml
+  set_image_prefix ./k8s/ldap.yml
+  set_image_prefix ./k8s/nfs.yml
+  set_image_prefix ./k8s/rsp-ldap.yml
+}
+
+function get_k8s_images() {
+  grep -rnw ./k8s -e 'image:' --exclude=helpers.sh --exclude=*.bk
+}
+
 # TODO: Helpers for `port-forward` and `get -o yaml` that use a regex?
