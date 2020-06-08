b53106f4513a6bcc318abc4c16f9be5569de1bf8
Cole Arendt <cole.arendt@outlook.com>
2020-06-08 07:14:59 -0400

    modernize ssl-connect image

--- cluster/ssl-connect/start-licensed.sh
+++ cluster/ssl-connect/start-licensed.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
-if [ -z "$CONNECT_LICENSE" ]; then
-    echo >&2 'error: The CONNECT_LICENSE variable is not set.'
+if [ -z "$RSC_LICENSE" ]; then
+    echo >&2 'error: The RSC_LICENSE variable is not set.'
     "$@"
     exit 1
 fi
 
 
 activate() {
-    echo "Activating license ..."
-    sudo /opt/rstudio-connect/bin/license-manager activate $CONNECT_LICENSE # output is informative.
-    if [ $? -ne 0 ]
-    then
-        echo >&2 'error: CONNECT_LICENSE could not be activated.'
-        exit 1
-    fi     
+    #echo "Activating license ..."
+    #sudo /opt/rstudio-connect/bin/license-manager activate $RSC_LICENSE # output is informative.
+    #if [ $? -ne 0 ]
+    #then
+    #    echo >&2 'error: RSC_LICENSE could not be activated.'
+    #    exit 1
+    #fi     
+
+    # Activate License
+    if ! [ -z "$RSC_LICENSE" ]; then
+        /opt/rstudio-connect/bin/license-manager activate $RSC_LICENSE
+    elif ! [ -z "$RSC_LICENSE_SERVER" ]; then
+        /opt/rstudio-connect/bin/license-manager license-server $RSC_LICENSE_SERVER
+    elif test -f "/etc/rstudio-connect/license.lic"; then
+        /opt/rstudio-connect/bin/license-manager activate-file /etc/rstudio-connect/license.lic
+    fi
 }
 
 deactivate() {
@@ -28,9 +37,11 @@ trap deactivate EXIT
 
 echo 'Executing SSL script'
 # set up SSL
-/cert_get.sh
+/cert_get.sh http://cfssl:8888 ssl-connect
+
+# lest this be inherited by child processes
+unset RSC_LICENSE
+unset RSC_LICENSE_SERVER
 
 echo "Starting connect ..."
-"$@"
-STATUS="$?"
-
+/opt/rstudio-connect/bin/connect --config /etc/rstudio-connect/rstudio-connect.gcfg
