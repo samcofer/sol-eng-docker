41f0c5a31050700b34af890e1586866a930ed9e2
Cole Arendt <cole.arendt@outlook.com>
2020-06-25 16:29:28 -0400

    update the load test image

--- cluster/load-test/entrypoint.sh
+++ cluster/load-test/entrypoint.sh
@@ -3,15 +3,33 @@
 set -e
 set -x
 
+mkdir -p /output/
+
 # Deactivate license when it exists
 deactivate() {
-    echo "== Exiting =="
+    echo "== Trapped Exit Command =="
 
-    echo "Writing output to S3"
-    aws s3 cp /output/ ${S3_BUCKET} --acl bucket-owner-full-control --recursive
+    echo "--> Killing java forcefully"
+    kill -9 `pidof java`
+
+    if [ -n "${S3_BUCKET}" ]; then
+      echo "--> Writing output to S3"
+      aws s3 sync /output/ "${S3_BUCKET}" --acl bucket-owner-full-control
+    else
+      echo "--> No S3 bucket defined. Not writing output"
+    fi
 
     echo "== Done =="
+    exit
 }
 trap deactivate EXIT
 
-/usr/local/bin/shinycannon $@
+if [ -n "${S3_RECORDING_LOCATION}" ]; then
+  echo "--> Getting recording.log from S3"
+  aws s3 cp "${S3_RECORDING_LOCATION}" /recording.log
+elif [ -n "${RECORDING_LOCATION}" ]; then
+  echo "--> Getting recording.log from the internet"
+  curl -o /recording.log "${RECORDING_LOCATION}"
+fi
+
+/usr/local/bin/shinycannon $@ --output-dir /output/$(date +'%s')-${HOSTNAME}
