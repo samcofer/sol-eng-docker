78b9b805ed91a638dd1f0011389008c5bf37441b
Cole Arendt <cole.arendt@outlook.com>
2020-12-08 15:15:15 -0500

    switch kerberos-ssh to a statefulset

--- cluster/ssh-server/config_ssh_krb5.sh
+++ cluster/ssh-server/config_ssh_krb5.sh
@@ -2,7 +2,7 @@
 set -ex
 kadmin -p ubuntu/admin -w ubuntu -q "addprinc -randkey host/kerberos-ssh-server"
 kadmin -p ubuntu/admin -w ubuntu -q "ktadd -k /etc/krb5.keytab host/kerberos-ssh-server"
-kadmin -p ubuntu/admin -w ubuntu -q "addprinc -randkey host/$(hostname)"
-kadmin -p ubuntu/admin -w ubuntu -q "ktadd -k /etc/krb5.keytab host/$(hostname)"
+kadmin -p ubuntu/admin -w ubuntu -q "addprinc -randkey host/$(hostname --fqdn)"
+kadmin -p ubuntu/admin -w ubuntu -q "ktadd -k /etc/krb5.keytab host/$(hostname --fqdn)"
 kadmin -p ubuntu/admin -w ubuntu -q "addprinc -randkey host/kerberos-ssh-server.default.svc.cluster.local"
 kadmin -p ubuntu/admin -w ubuntu -q "ktadd -k /etc/krb5.keytab host/kerberos-ssh-server.default.svc.cluster.local"
