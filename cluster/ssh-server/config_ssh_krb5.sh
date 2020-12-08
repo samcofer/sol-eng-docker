598e667554aa089695458933e8fae55253e9e15d
Ricardo Andrade <ricfand@gmail.com>
2020-12-08 12:02:12 -0600

    trying to make kerb work over k8s domains

--- cluster/ssh-server/config_ssh_krb5.sh
+++ cluster/ssh-server/config_ssh_krb5.sh
@@ -1,2 +1,4 @@
 kadmin -p ubuntu/admin -w ubuntu -q "addprinc -randkey host/kerberos-ssh-server"
 kadmin -p ubuntu/admin -w ubuntu -q "ktadd -k /etc/krb5.keytab host/kerberos-ssh-server"
+kadmin -p ubuntu/admin -w ubuntu -q "addprinc -randkey host/kerberos-ssh-server.default.svc.cluster.local"
+kadmin -p ubuntu/admin -w ubuntu -q "ktadd -k /etc/krb5.keytab host/kerberos-ssh-server.default.svc.cluster.local"
