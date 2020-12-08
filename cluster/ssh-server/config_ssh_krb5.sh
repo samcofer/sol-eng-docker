32677348c275465f0c32c5379f1a6791ee520a43
Cole Arendt <cole.arendt@outlook.com>
2020-12-08 14:16:46 -0500

    update ssh-server to be more verbose

--- cluster/ssh-server/config_ssh_krb5.sh
+++ cluster/ssh-server/config_ssh_krb5.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+set -ex
 kadmin -p ubuntu/admin -w ubuntu -q "addprinc -randkey host/kerberos-ssh-server"
 kadmin -p ubuntu/admin -w ubuntu -q "ktadd -k /etc/krb5.keytab host/kerberos-ssh-server"
+kadmin -p ubuntu/admin -w ubuntu -q "addprinc -randkey host/$(hostname)"
+kadmin -p ubuntu/admin -w ubuntu -q "ktadd -k /etc/krb5.keytab host/$(hostname)"
 kadmin -p ubuntu/admin -w ubuntu -q "addprinc -randkey host/kerberos-ssh-server.default.svc.cluster.local"
 kadmin -p ubuntu/admin -w ubuntu -q "ktadd -k /etc/krb5.keytab host/kerberos-ssh-server.default.svc.cluster.local"
